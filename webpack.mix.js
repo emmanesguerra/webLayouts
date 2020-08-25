a454dea38ef900595b1c1a0ae9236328f6986103
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-25 09:56:42 +0800

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.setPublicPath('Food');
+
+mix.js('Food/resources/js/app.js', 'js')
+    .sass('Food/resources/sass/app.scss', 'css');
