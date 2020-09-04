0a265d354537874631d3c04b78af4c0945879b9b
emmanesguerra <emman.esguerra2013@gmail.com>
2020-09-04 13:26:49 +0800

    fix homepage

--- Health/resources/js/app.js
+++ Health/resources/js/app.js
@@ -1,1 +1,24 @@
 require('./bootstrap');
+
+
+import Swiper from 'swiper/bundle';
+
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+// init Swiper:
+const swiper = new Swiper('.swiper-container', {
+    effect: 'fade',
+    direction: 'horizontal',
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    fadeEffect: {
+        crossFade: true
+    },
+});
